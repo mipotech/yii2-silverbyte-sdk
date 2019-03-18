2275d8d314548be4bbdefa32539360a0b563eb35
Mipo-7-Fattal <team@mipo.co.il>
2019-03-18 12:45:01 +0200

    AlertEmail remove HEAD tag

--- AlertEmail.php
+++ AlertEmail.php
@@ -20,11 +20,7 @@ class AlertEmail
 		if (isset($extraParams['recipients'])) {
 			$to = $extraParams['recipients'];
 		}
-<<<<<<< HEAD
-		elseif ($recipients = \app\modules\dashboard\components\HelperFunc::getSiteSetting('sb_email_recipient_list')) {
-=======
 		elseif ($recipients = HelperFunc::getSiteSetting('sb_email_recipient_list')) {
->>>>>>> faa5ee8770b17c60b0fe407f7d86f378a0710aee
 			$to = $recipients;
 		}
 		elseif (isset(Yii::$app->params['silverbyte']['alertEmailRecipients'])) {
@@ -35,11 +31,7 @@ class AlertEmail
 		}
 
 		$ret = Yii::$app->mailer->compose()
-<<<<<<< HEAD
 			->setFrom(['no-reply@fattal-hotels.com'=>'Fattal Hotels Website'])
-=======
-			->setFrom('no-reply@fattal-hotels.com')
->>>>>>> faa5ee8770b17c60b0fe407f7d86f378a0710aee
 			->setTo($to)
 			->setSubject($subject)
 			->setHtmlBody($body)
